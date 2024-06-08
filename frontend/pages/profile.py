20a4618f8ecdd25acff8befaeca099d2c1d8cb21
Gultac's ceyran <gultaj.seyid@bhos.edu.az>
2024-06-08 17:13:53 +0400

    'profile'

--- frontend/pages/profile.py
+++ frontend/pages/profile.py
@@ -3,16 +3,17 @@ from time import sleep
 from navigation import make_sidebar
 
 make_sidebar()
-
+a = 2
 st.write(
     """
-#  Your Profile
-
-This is a secret page that only logged-in users can see.
-
-Super duper secret.
-
-Shh....
-
+#  Your Profile 🤠
+### Name {user_name, user_surname}
+### Account created {create_date}
+### Streaks {current_streak}🔥
+### Longest streaks {longest_streak} 🔥
+### Badges:
 """
 )
+st.image(['images/good_listener.png', 'images/good_streaker.png', 'images/lazy_learner.png'], width=200, caption=['Good Listener', 'Streaks maker', 'Lazy learner'])
+
+# * optional kwarg unsafe_allow_html = True
